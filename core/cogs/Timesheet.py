99d746faf7151075f87bee2bd85d51f69f756194
Lore <30599612+Lorenyx@users.noreply.github.com>
2021-04-20 13:54:42 -0500

    Update Timesheet

--- core/cogs/Timesheet.py
+++ core/cogs/Timesheet.py
@@ -3,7 +3,7 @@ import gspread
 from discord.ext import commands
 from datetime import datetime
 
-from constants import HEADER, gsheet_token
+from constants import HEADER, gs_token
 
 class Timesheet(commands.Cog):
     """Handles the time and connection to the google sheet"""
@@ -18,8 +18,14 @@ class Timesheet(commands.Cog):
 
     async def get_wks(self):
         try:
-            wks = gsheet.worksheet(self.cmonth())
+            wks = gs_token.worksheet(self.cmonth())
         except gspread.WorksheetNotFound:
-            wks = gsheet.add_worksheet(self.cmonth(), 10, 5)
+            wks = gs_token.add_worksheet(self.cmonth(), 10, 5)
             wks.append_row(HEADER, table_range='A1:D1')
         return wks
+
+    @commands.command(alias='in')
+    async def clock_in(self, ctx):
+        #TODO Check if clocked in/out
+        #TODO find next line
+        pass
